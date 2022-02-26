09fd8a6df9b6345706ddceefe90e8b5daacbfb85
LoadingBG <troyankru@gmail.com>
2022-02-26 22:22:38 +0200

    Fix short ranges

--- src/main/java/up/stream/ShortRangeStream.java
+++ src/main/java/up/stream/ShortRangeStream.java
@@ -7,21 +7,30 @@ final class ShortRangeStream extends Stream<Short> {
     private final short end;
     private final short step;
     private short curr;
+    private boolean hasWrapped;
 
     ShortRangeStream(final short start, final short end, final short step) {
         this.start = start;
         this.end = end;
         this.step = step;
         curr = start;
+        hasWrapped = false;
     }
 
     @Override
     protected Optional<Short> next() {
-        if (curr > end) {
+        if (hasWrapped || curr >= end) {
             return Optional.empty();
         }
+
         final Optional<Short> res = Optional.of(curr);
+
+        final boolean wasPositive = curr > 0;
         curr += step;
+        if (wasPositive && curr < 0) {
+            hasWrapped = true;
+        }
+
         return res;
     }
 
